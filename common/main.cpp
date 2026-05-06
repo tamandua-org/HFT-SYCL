4223486eeb49a2d74be8a39a34048cf05803b485
Tufo88 <a.armengol2004@gmail.com>
2026-05-06 19:10:30 +0200

    fix algorithm and cleanup stock info

--- common/main.cpp
+++ common/main.cpp
@@ -20,9 +20,11 @@ void readAsset(std::vector<float> &prices, const std::string &filename) {
   }
 }
 
-int loadAllAssets(std::array<std::vector<float>, N_STOCKS> &assets) {
-  std::string folder = "./inputs"; // importante: la llamada a esta funcion hay
-                                   // que hacerla desde HFT-SYCL
+void loadAllAssets(std::array<std::vector<float>, N_STOCKS> &assets,
+                  std::array<std::string, N_STOCKS> &stockNames) {
+  std::string folder = "../inputs"; // importante: la llamada a esta funcion hay
+                                    // que hacerla desde un directorio de build
+                                    // (o cualquier otra subcarpeta)
   int i = 0;
   int minAssetCount = INT_MAX;
 
@@ -33,21 +35,27 @@ int loadAllAssets(std::array<std::vector<float>, N_STOCKS> &assets) {
 
       std::vector<float> prices;
       readAsset(prices, path);
-      assets[i] = prices;
+      stockNames[i] = path.substr(folder.length() + 1, path.length() - folder.length() + 1);
+      assets[i++] = prices;
 
       minAssetCount = std::min(minAssetCount, static_cast<int>(prices.size()));
+
+      if (i == N_STOCKS)
+        break;
     }
   }
 
-  return minAssetCount;
+  for (auto &stock : assets) {
+    stock.resize(minAssetCount);
+  }
+
 }
 
 std::vector<Tick> buildTicks(std::array<std::vector<float>, N_STOCKS> &assets,
-                             std::array<std::string, N_STOCKS> &stockNames,
-                             int numTicks) {
+                             std::array<std::string, N_STOCKS> &stockNames) {
   std::vector<Tick> ticks;
 
-  for (int t = 0; t < numTicks; t++) {
+  for (int t = 0; t < assets[0].size(); t++) {
     Tick tick;
 
     for (std::size_t a = 0; a < N_STOCKS; a++) {
@@ -64,13 +72,14 @@ int main() {
   std::array<std::vector<float>, N_STOCKS> assets;
   std::array<std::string, N_STOCKS> stockNames;
 
-  int minTicks = loadAllAssets(assets);
+  loadAllAssets(assets, stockNames);
 
-  auto ticks = buildTicks(assets, stockNames, minTicks);
+  auto ticks = buildTicks(assets, stockNames);
 
   auto positions = runTicks(ticks);
+
   for (const auto &[stock, pos] : std::views::zip(stockNames, positions)) {
-    std::cout << stock << " " << pos;
+    std::cout << stock << " " << pos << '\n';
   }
 
   return 0;
