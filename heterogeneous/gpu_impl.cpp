61695f38bf113cc544bd7ac565282fe357d45434
zhengzhu14 <zheng.k.zhu@gmail.com>
2026-05-06 14:31:37 +0200

    Pequeño cambio

--- heterogeneous/gpu_impl.cpp
+++ heterogeneous/gpu_impl.cpp
@@ -390,19 +390,5 @@ void runTicks(const std::vector<Tick> &ticks) {
     sycl::free(shared_ticks, Q);
     sycl::free(shared_pairs, Q);
 
-
-
-
-void runTicks(const std::vector<Tick> &ticks) {
-    //Implementación en GPU
-
-    //1. Convertir la matriz de ticks por stock a stock por asset
-
-    //2. Construir ventana inicial
-
-    //3. Analizar los ticks nuevos
-
-
-
-
 }
+
