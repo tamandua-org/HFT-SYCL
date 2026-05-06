ca3b0f2d95a05432f5b1485efc420e33ea5a012a
zhengzhu14 <zheng.k.zhu@gmail.com>
2026-05-06 18:12:50 +0200

    Quitado cosas innecesarias

--- common/pair_info.hpp
+++ common/pair_info.hpp
@@ -10,12 +10,3 @@ struct PairInfo {
     float meanSpread = 0.0f, varSpread = 0.0f;
     int8_t position = HOLD;
 };
-
-// SoA "plana" pensada para vivir en registros del hilo durante el kernel.
-// uint16_t en i,j permite hasta 65 535 stocks.
-struct FlatPairInfo {
-    float    meanI, meanJ, varJ, covIJ;
-    float    meanSpread, varSpread, beta;
-    uint16_t i, j;
-    int8_t   position;
-};
