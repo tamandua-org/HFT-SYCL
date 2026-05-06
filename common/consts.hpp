323a74fc93966b3c152f34c4356f7aec014cdb24
zhengzhu14 <zheng.k.zhu@gmail.com>
2026-05-06 11:34:56 +0200

    Cambio en el algoritmo que incluye un warup inicial

--- common/consts.hpp
+++ common/consts.hpp
@@ -2,6 +2,7 @@
 
 constexpr uint32_t N_STOCKS = 16;
 constexpr uint32_t WINDOW_SIZE = 32;
+constexpr uint32_t N_TICKS = 256;
 
 constexpr float ALPHA = 0.05f;
 constexpr float EPSILON = 1e-6f;
--- common/pair_info.hpp
+++ common/pair_info.hpp
@@ -5,14 +5,26 @@
 #include <array>
 
 constexpr int8_t HOLD = 0;
-constexpr int8_t BUY = 0;
-constexpr int8_t SELL = 0;
+constexpr int8_t BUY = 1;
+constexpr int8_t SELL = -1;
 
 struct PairInfo
 {
-    float meanSpread, varSpread;
-    std::array<float, WINDOW_SIZE> window;
-    float sum, sumSq, meanI, meanJ, varJ, covIJ, beta;
+    // float meanSpread = 0.0f;
+    // float varSpread = 0.0f;
+    //std::array<float, WINDOW_SIZE> window;
+    //float sumSq, meanI, meanJ, varJ, covIJ, beta;
+    //int8_t position = HOLD;
 
-    int8_t position;
+    float meanI    = 0.0f;
+    float meanJ    = 0.0f;
+    float varJ     = 0.0f;
+    float covIJ    = 0.0f;
+    float beta     = 0.0f;
+    float meanSpread = 0.0f;
+    float varSpread  = 0.0f;
+    int8_t   position  = HOLD;
+    uint16_t tickCount = 0;
+    bool warmedup = false;
+
 };
